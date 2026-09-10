e177d297727467b5b04742a69cb50336bd36b04c
usersaurabh45 <saurabhpal.89489@gmail.com>
2026-09-10 16:04:01 +0530

    Object

---
+++ objectLiteral.js
@@ -0,0 +1,17 @@
+let name = "Saurabh";
+let Roll = 101;
+
+// Normal way
+let student = {
+    name: name,
+    Roll: Roll
+};
+
+// Shorthand property
+let student2 = {
+    name,
+    Roll
+};
+
+console.log(student2.name);
+console.log(student2.Roll);
